ad28cb2a444ac57d8b864a1d64ab53dc0a423c05
unglevan <huythang0312@gmail.com>
2013-11-21 18:53:16 +0900

    add init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <div>
+     <?php
+        for($i = 0; $i< 5000; $i++)
+        {
+        ?>
+        <img src="images/spdy.png"/>
+       <?php
+       }
+       ?>
+
+    </div>
+</body>
+</html>
